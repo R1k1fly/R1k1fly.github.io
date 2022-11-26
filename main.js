637287ad4531b33f1f356be1ff609513e3514971
R1k1fly <andreysvj@gmail.com>
2022-11-26 11:15:45 +0100

    Forgot to change the title

--- main.js
+++ main.js
@@ -27,53 +27,69 @@ function winner(){
   if (document.getElementById("box_1").innerHTML == "X" && document.getElementById("box_2").innerHTML == "X"  && document.getElementById("box_3").innerHTML == "X"){
     document.getElementById("player").sty
     document.getElementById("player").innerHTML = "Player 1 wins!";
+        alert("Congratulations, Player 1!");
     
 }else if (document.getElementById("box_4").innerHTML == "X" && document.getElementById("box_5").innerHTML == "X"  && document.getElementById("box_6").innerHTML == "X"){
     document.getElementById("player").innerHTML = "Player 1 wins!";
+            alert("Congratulations, Player 1!");
     
 }else if (document.getElementById("box_7").innerHTML == "X" && document.getElementById("box_8").innerHTML == "X"  && document.getElementById("box_9").innerHTML == "X"){
     document.getElementById("player").innerHTML = "Player 1 wins!";
+            alert("Congratulations, Player 1!");
     
 }else if (document.getElementById("box_1").innerHTML == "X" && document.getElementById("box_4").innerHTML == "X"  && document.getElementById("box_7").innerHTML == "X"){
     document.getElementById("player").innerHTML = "Player 1 wins!";
+            alert("Congratulations, Player 1!");
     
 }else if (document.getElementById("box_2").innerHTML == "X" && document.getElementById("box_5").innerHTML == "X"  && document.getElementById("box_8").innerHTML == "X"){
     document.getElementById("player").innerHTML = "Player 1 wins!";
     
+            alert("Congratulations, Player 1!");
 }else if (document.getElementById("box_3").innerHTML == "X" && document.getElementById("box_6").innerHTML == "X"  && document.getElementById("box_9").innerHTML == "X"){
     document.getElementById("player").innerHTML = "Player 1 wins!";
+            alert("Congratulations, Player 1!");
     
 }else if (document.getElementById("box_1").innerHTML == "X" && document.getElementById("box_5").innerHTML == "X"  && document.getElementById("box_9").innerHTML == "X"){
     document.getElementById("player").innerHTML = "Player 1 wins!";
+            alert("Congratulations, Player 1!");
     
 }else if (document.getElementById("box_3").innerHTML == "X" && document.getElementById("box_5").innerHTML == "X"  && document.getElementById("box_7").innerHTML == "X"){
     document.getElementById("player").innerHTML = "Player 1 wins!";
+            alert("Congratulations, Player 1!");
     
 }
 
 else if (document.getElementById("box_4").innerHTML == "O" && document.getElementById("box_5").innerHTML == "O"  && document.getElementById("box_6").innerHTML == "O"){
     document.getElementById("player").innerHTML = "Player 2 wins!";
+    alert("Congratulations, Player 2!");
     
 }else if (document.getElementById("box_7").innerHTML == "O" && document.getElementById("box_8").innerHTML == "O"  && document.getElementById("box_9").innerHTML == "O"){
     document.getElementById("player").innerHTML = "Player 2 wins!";
+        alert("Congratulations, Player 2!");
     
 }else if (document.getElementById("box_1").innerHTML == "O" && document.getElementById("box_4").innerHTML == "O"  && document.getElementById("box_7").innerHTML == "O"){
     document.getElementById("player").innerHTML = "Player 2 wins!";
+        alert("Congratulations, Player 2!");
 
 }else if (document.getElementById("box_2").innerHTML == "O" && document.getElementById("box_5").innerHTML == "O"  && document.getElementById("box_8").innerHTML == "O"){
     document.getElementById("player").innerHTML = "Player 2 wins!";
+        alert("Congratulations, Player 2!");
 
 }else if (document.getElementById("box_3").innerHTML == "O" && document.getElementById("box_6").innerHTML == "O"  && document.getElementById("box_9").innerHTML == "O"){
     document.getElementById("player").innerHTML = "Player 2 wins!";
+        alert("Congratulations, Player 2!");
 
 }else if (document.getElementById("box_1").innerHTML == "O" && document.getElementById("box_5").innerHTML == "O"  && document.getElementById("box_9").innerHTML == "O"){
     document.getElementById("player").innerHTML = "Player 2 wins!";
+        alert("Congratulations, Player 2!");
 
 }else if (document.getElementById("box_3").innerHTML == "O" && document.getElementById("box_5").innerHTML == "O"  && document.getElementById("box_7").innerHTML == "O"){
     document.getElementById("player").innerHTML = "Player 2 wins!";
+        alert("Congratulations, Player 2!");
 } else{
     if (document.getElementById("box_1").innerHTML != "" && document.getElementById("box_2").innerHTML != ""  && document.getElementById("box_3").innerHTML != "" && document.getElementById("box_4").innerHTML != "" && document.getElementById("box_5").innerHTML != ""  && document.getElementById("box_6").innerHTML != "" && document.getElementById("box_7").innerHTML != "" && document.getElementById("box_8").innerHTML != ""  && document.getElementById("box_9").innerHTML != ""){
         document.getElementById("player").innerHTML = "Draw!";
+        alert("Draw!");
     }
 }
 }
